80fb50d39c3017d3b126606f3cb1c30e17a9991c
AMaldonadoLiu <92129710+AMaldonadoLiu@users.noreply.github.com>
2023-03-23 23:10:34 -0700

    Update Testbench.sv

--- Testbench.sv
+++ Testbench.sv
@@ -9,12 +9,16 @@ parameter N = 256;
   reg sel;
   wire [N-1:0] OUT;
   wire valid_key;
+
   reg [N-1:0] temp;
+
   string string_input;
   string temp_output;
+
   int verbose;
   int test_cases;
   int j;
+  int error_code;
 
 MTE #(.N(N)) TP (clock, key, IN, sel, OUT, valid_key);
 
@@ -22,22 +26,21 @@ class rand_string;
 	rand byte input_char[];
 	rand bit [255:0] password;
 
-	constraint str_len {input_char.size() == 257;}
-	constraint no_eof { foreach(input_char[i])
+	constraint str_len {input_char.size() == 32;}
+	//remove strange characters such as DEL and eot
+	constraint no_weird_char { foreach(input_char[i])
 				input_char[i] inside {[32:126]};
 			}
-	//constraint end_string { input_char[256] == 0;}
+	
 
 	//this function generates the string from the line of characters from the input_char
 	function string gen_string();
-		string input_string;
+		string input_string = "";
 		//go through every byte and add to string.
 		foreach(input_char[i])
 			input_string = {input_string, string'(input_char[i])};
-		//input_string.putc(256, 0);
-		//input_string.putc(0, 100);
-		//input_string.putc(1, 0);
-		//$display("Here is the string: %s", input_string);
+		
+
 		return input_string;
 	endfunction
 
@@ -59,49 +62,54 @@ initial
   if($test$plusargs("EXTENSIVE"))
 	test_cases = 100000;
   string_class = new;
-  string_class.randomize();
-  string_input = string_class.gen_string();
-  //$display("rand_string value: %h", string_class.input_char);
+  
 
-//@(negedge clock);
+
   for(j = 0; j < test_cases; j++)
 	begin
-	  string_class.randomize();
+	  if(!string_class.randomize())
+		begin
+		$display("Error randomizing!");
+		$finish;
+		end
 	  string_input = string_class.gen_string();
 	  if(verbose)
 		begin
 	  	$display("******Test Encrypt******"); //check that it encrypts
-	  	$display("Input: %s", string_input);
-		$display("Key: %d", string_class.password);
+	  	$display("Input: %s\n", string_input);
+		$display("Key: %d\n", string_class.password);
 		end
 	  sel = 1'b1; //encrypt
 	  key = string_class.password; 
-	  for(int i = 0; i < 32; i++)
+	  for(int i = 0; i < N / 8; i++)
 		IN = (IN << 8) | string_input.getc(i);
-	  //e_data1 = {32{8'hfe}};
+	  
 	  if(verbose)
-	  	$display("input is: %h", IN);
+	  	$display("input is: %h\n", IN);
 	
 	  repeat(10) @(negedge clock); //wait some time for valid data (need to actually find out exact number of clock cycles required)
 	  if(verbose)
 		begin  
 		temp_output = "";
-	  	for(int i = 0; i < 32; i++)
-		temp_output = {temp_output, string'(OUT[8*i+:7])};
+	  	for(int i = 0; i < N/8; i++)
+			temp_output = {temp_output, string'(OUT[8*i+:7])};
 	  	$display("output: %h\nString output: %s", OUT, temp_output); //output data to determine if correct
 	
-	  	$display("******Test Decrypt******"); //check that it decrypts
+	  	$display("\n******Test Decrypt******"); //check that it decrypts
 		end
 	  sel = 1'b0;
-	  //key = 'h13;
-	  //IN = OUT;
-	  //e_data1 = {32{8'hfe}};
-	  //repeat (20) @(negedge clock);
+	  
 	  #0 //update the clock so we get decrypted data instead of encrypted (shouldn't do this in final design)
 	  if(OUT !== IN)
 		$display("Decrypted wrong.");
 	  if(verbose)
-		$display("output: %h\n\n\n", OUT); //output decrypted data (later set this to check if the output is the same as the first input for automation);
+		begin
+		$display("output: %h\n", OUT); //output decrypted data (later set this to check if the output is the same as the first input for automation);
+		temp_output = "";
+		for(int i = 0; i < N/8; i++)
+			temp_output = {temp_output, string'(OUT[8*i+:7])};
+		$display("Output String: %s\n\n\n", temp_output);
+		end
 	end
 
 
