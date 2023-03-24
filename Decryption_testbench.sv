41d24e2522da5c8744cb2c0211135dab879d9432
BhuvanYadav1 <126542244+BhuvanYadav1@users.noreply.github.com>
2023-03-24 16:13:44 -0700

    Update Decryption_testbench.sv

--- Decryption_testbench.sv
+++ Decryption_testbench.sv
@@ -1,10 +1,14 @@
+
 module top;
-parameter N= 8;
+  parameter N= 8;
+  parameter NRANDOM = 1000;
   
-  bit clock;
+  reg clock;
   reg [N-1:0]key;
   reg [N-1:0]e_data;
+  reg [N-1:0]MAC;
   wire [N-1:0]data;
+  wire [N-1:0]e_MAC;
 
   decryption DUT(clock,key,e_data,data);
 
@@ -14,29 +18,16 @@ forever #5 clock = ~clock;
 end
 
 initial begin
-
+  $monitor("key:%h e_data:%h data:%h",key,e_data,data);
 //@(negedge clock);
-  key = 8'd0;
-  e_data = 8'd1;
-
-
-  @(negedge clock);
-  key = 8'hF;
-  e_data = 79;
-
-
-@(negedge clock);
-  key = 8'b10100000;
-  e_data = 80;
- 
-  repeat(4)@(negedge clock);
-  $display("data: ",data);
-  repeat(15)
-	begin
-	@(negedge clock);
-	$display("data: ",data);
-	end
-
+  
+  repeat(NRANDOM)
+    begin
+    key = $random;
+    e_data = $random;
+      @(negedge clock);
+    end
+  
   repeat(20) @(negedge clock);
 $finish;
 end
@@ -47,3 +38,4 @@ end
   end
   
 endmodule
+
